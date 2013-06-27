cd6286191ead070efe628d5fa8554f9c671b2ffb
Ben Mather <bwhmather@gmail.com>
2013-06-27 19:42:04 +0100

    Update tests to match changed fields api

--- pyixp/tests/test_fields.py
+++ pyixp/tests/test_fields.py
@@ -16,54 +16,50 @@ class IdentityTest(unittest.TestCase):
         self.assertEqual(value, unpacked)
 
     def test_uint(self):
-        uint16 = fields.UInt(2)
-        self.pack_and_unpack(uint16, 0)
-        self.pack_and_unpack(uint16, 0xffff)
-        self.pack_and_unpack(uint16, 0xff00)
-        self.pack_and_unpack(uint16, 0x0123)
+        self.pack_and_unpack(fields.uint16, 0)
+        self.pack_and_unpack(fields.uint16, 0xffff)
+        self.pack_and_unpack(fields.uint16, 0xff00)
+        self.pack_and_unpack(fields.uint16, 0x0123)
 
-        uint32 = fields.UInt(4)
-        self.pack_and_unpack(uint32, 0)
-        self.pack_and_unpack(uint32, 0xffffffff)
-        self.pack_and_unpack(uint32, 0xffff0000)
-        self.pack_and_unpack(uint32, 0x01234567)
-        self.pack_and_unpack(uint32, 0x89abcdef)
+        self.pack_and_unpack(fields.uint32, 0)
+        self.pack_and_unpack(fields.uint32, 0xffffffff)
+        self.pack_and_unpack(fields.uint32, 0xffff0000)
+        self.pack_and_unpack(fields.uint32, 0x01234567)
+        self.pack_and_unpack(fields.uint32, 0x89abcdef)
 
-        uint64 = fields.UInt(8)
-        self.pack_and_unpack(uint64, 0)
-        self.pack_and_unpack(uint64, 0xffffffffffffffff)
-        self.pack_and_unpack(uint64, 0xffff0000ffff0000)
-        self.pack_and_unpack(uint64, 0x0123456789abcded)
-        self.pack_and_unpack(uint64, 0x89abcdef01234567)
+        self.pack_and_unpack(fields.uint64, 0)
+        self.pack_and_unpack(fields.uint64, 0xffffffffffffffff)
+        self.pack_and_unpack(fields.uint64, 0xffff0000ffff0000)
+        self.pack_and_unpack(fields.uint64, 0x0123456789abcded)
+        self.pack_and_unpack(fields.uint64, 0x89abcdef01234567)
 
     def test_data(self):
-        data = fields.Data(2)
+        data = fields.Data(fields.uint16)
         self.pack_and_unpack(data, b"Hello World")
 
     def test_string(self):
-        string2 = fields.String(2)
+        string2 = fields.String(fields.uint16)
 
         self.pack_and_unpack(string2, "Hello World")
 
     def test_array(self):
         # elements with fixed length
-        uint32 = fields.UInt(4)
-        uint32_array = fields.Array(2, uint32)
+        uint32_array = fields.Array(fields.uint16, fields.uint32)
         self.pack_and_unpack(uint32_array, [])
         self.pack_and_unpack(uint32_array, [1234, 5678, 91011])
 
         # elements with variable length
-        string = fields.String(2)
-        string_array = fields.Array(2, string)
+        string = fields.String(fields.uint16)
+        string_array = fields.Array(fields.uint16, string)
         self.pack_and_unpack(string_array, ["hello", "world", "!"])
 
     def test_sequence(self):
-        seq = fields.Sequence(fields.UInt(2), fields.String(2), fields.UInt(4))
+        seq = fields.Sequence(fields.uint16, fields.String(), fields.uint32)
         self.pack_and_unpack(seq, (5, "test string", 9,))
 
     def test_struct(self):
         struct = fields.Struct(
-            ("foo", fields.UInt(2),),
-            ("bar", fields.String(2),),
-            ("baz", fields.UInt(4),))
+            ("foo", fields.uint16,),
+            ("bar", fields.String(),),
+            ("baz", fields.uint32,))
         self.pack_and_unpack(struct, {"foo": 5, "bar": "test", "baz": 9})
